812cc54ed21e1fdc33dbddec89ffd4d10af64189
SebNap <1954404795@qq.com>
2023-03-20 20:14:03 +0800

    server demo3 规范化

---
+++ src/main/java/com/gerty/springboot/controller/wyqUserController.java
@@ -0,0 +1,25 @@
+package com.gerty.springboot.controller;
+
+
+import com.gerty.springboot.entity.wyqUser;
+import com.gerty.springboot.mapper.wyqUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class wyqUserController {
+
+    @Autowired
+    private wyqUserMapper userMapper;
+
+    @GetMapping("/")
+    public List<wyqUser> index(){
+        wyqUser user = new wyqUser();
+        user.setId(1);
+
+        return userMapper.findAll();
+    }
+}
